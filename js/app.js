0ed3dfe25b18cfcbe5cfbda27b5ec31c31363374
Amin <78924745+Aminchik86@users.noreply.github.com>
2022-03-22 15:11:43 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function openMenu() {
+    document.getElementById ("slidebar").classList.toggle("active")
+}
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID). scrollIntoView ({
+            behavior:"smooth",
+            block:"start"
+        })
+    })
+}
